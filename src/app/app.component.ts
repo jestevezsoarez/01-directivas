a88c89648350bae26aa28060874343a95a6c3899
Javier Estevez <javierdaniloestevez@gmail.com>
2021-01-29 05:22:31 -0300

    probando variables con interpolacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app1';
+  
+  nombre: string = "Javier Estevez";
+  edad: number = 47;
 }
