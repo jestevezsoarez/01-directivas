5434ce425840f8912fe1b46c6048c6581d3750cb
Javier Estevez <javierdaniloestevez@gmail.com>
2021-01-30 01:43:59 -0300

    creacion header componente de forma manual

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { from } from "rxjs";
+import { Component } from '@angular/core';
+ 
+@Component({
+    selector: 'app-header',
+    template: `<h1>Header Component</h1>`
+})
+export class HeaderComponent {
+
+
+}
