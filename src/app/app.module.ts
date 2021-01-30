91cabd9dff7e69ff58112961e9c75b419c91c937
Javier Estevez <javierdaniloestevez@gmail.com>
2021-01-30 05:18:07 -0300

    agregue body component y navbar en el header bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent} from './components/header/header.component'
+import { HeaderComponent} from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
